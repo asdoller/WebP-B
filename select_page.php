f9e53838e6d770036314914438e59d3a6d61b182
asdoller <76464692+asdoller@users.noreply.github.com>
2021-01-04 23:24:24 +0900

    Create select_page.php

---
+++ select_page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Select Page</title>
+    <link rel="stylesheet" href="select_page.css">
+</head>
+
+<?php
+function h($str) { return htmlspecialchars($str, ENT_QUOTES, "UTF-8"); }
+
+$temp = $_GET["temp"];
+$weather = $_GET["weather"];
+
+$pdo = new PDO("sqlite:webp.sqlite");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+$st = $pdo->prepare("INSERT INTO situation(temp,weather) VALUES(?, ?)");
+$st->execute(array($temp, $weather));
+?>
+
+<body>
+  <h1>Select Page</h1>
+    <form action="select_page.php" method="get">
+    Temparature<br>
+    <select name="temp">
+    <option value="-5">BELOW 5℃</option>
+    <option value="5-10">5℃~10℃</option>
+    <option value="10-15">10℃~15℃</option>
+    <option value="15-20">15℃~20℃</option>
+    <option value="20-25">20℃~25℃</option>
+    <option value="25-30">25℃~30℃</option>
+    <option value="30-">OVER 30℃</option>
+    </select> <br>
+    Weather<br>
+    <select name="weather">
+    <option value="sunny">☀︎SUNNY</option>
+    <option value="cloudy">☁︎CLOUDY</option>
+    <option value="rainy">☔︎RAINY</option>
+    </select><br><br>
+    <input type="submit" value="SEND"><br>
+    </form>
+</body>
